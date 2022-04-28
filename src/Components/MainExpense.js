afbb5325b8a0b4f1b0f0dbfdadb81a8e8c18ec6f
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2022-04-28 18:52:36 +0100

    added MainExpense.css for styling

--- src/Components/MainExpense.js
+++ src/Components/MainExpense.js
@@ -1,12 +1,13 @@
 import ExpenseItem from './ExpenseItem'
+import './MainExpense.css'
 
 export default function MainExpenseItem(props) {
   return (
-    <>
+    <div className='expenses'>
       <ExpenseItem data={props.data[0]}/>
       <ExpenseItem data={props.data[1]}/>
       <ExpenseItem data={props.data[2]}/>
       <ExpenseItem data={props.data[3]}/>
-    </>
+    </div>
   )
 }
